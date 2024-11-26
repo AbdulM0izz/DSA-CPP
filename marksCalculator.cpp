b850ca8d5e624ce0461ab20a1cb6c132ebc70281
Abdul Moiz <abdulmoiz95972@gmail.com>
2024-11-27 00:55:53 +0500

    Done

--- marksCalculator.cpp
+++ marksCalculator.cpp
@@ -1,16 +1,25 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main(){
-int marks;
-cin>>marks;
+	int marks;
+	cout<<"Enter your marks "<<endl;
+	cin>>marks;
 
-if(marks>33){
-    cout<<"pass"<<endl;
-}
-else{
-    cout<<"fail"<<endl;
+	if(marks>90){
+		cout<<" A "<<endl;
+	}else if(marks > 80 && marks <90){
+		cout<<" B "<<endl;
+	}else if(marks > 60 && marks < 80){
+		cout<<" C "<<endl;
+	}else{
+		cout<<"Fail "<<endl;
+	}
+	 
+	
+	
+	
+	
+	return 0;
+	
 }
 
-
-    return 0;
-}
