f7255cfa17a57f469a0bb3a5ad9a89a40537bc15
abdulmoiz95972 <abdulmoiz95972@gmail.com>
2025-04-08 03:09:26 +0500

    Subsequence string recursive

---
+++ Recursion/SubSequence.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+  
+void f (string str , int i , string result, vector <string>& li) {
+       if (i == str.length() ) {
+         li.push_back(result);
+         return;
+       }
+
+       f (str, i+1, result + str[i] , li);
+       f (str , i+1, result , li);
+}
+
+int main () {
+    //given a string find out all the subsequences
+  vector <string> res;
+  string str = "abc";
+  f (str, 0, "", res);
+
+  for (int i=0; i<res.size(); i++) {
+    cout << res[i] << " ";
+  }
+
+    return 0;
+}
