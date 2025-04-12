ae615cd01b43a6b0882c9ad47ad7d9a590511c6b
abdulmoiz95972 <abdulmoiz95972@gmail.com>
2025-04-12 20:58:04 +0500

    Selection Sort

--- Sorting-Algorithms/BubbleSort.cpp
+++ Sorting-Algorithms/BubbleSort.cpp
@@ -1,30 +1,33 @@
     #include <iostream>
     #include <vector>
     using namespace std;
-    int bubblesort (vector<int> &v) {
+    
+    void bubblesort (vector<int> &v) {
         for (int i=0; i<v.size(); i++) {
+            bool flag = false;
             for (int j=i+1; j<v.size(); j++) {
                 if (v[i] > v[j]){
+                    flag = true;
                     swap(v[i],v[j]);
                 }
             }
+            if (!flag) break;
         }
+        return ;
+    }
+        int main () {
+            int n;
+            cin >> n;
+            vector<int> v(n);
+
+            for (int i=0; i<v.size(); i++) {
+                cin >> v[i];
+            }
+            bubblesort(v);
+
+            for (int i=0; i<v.size(); i++) {
+                cout << v[i] << " ";
+            }
+                
             return 0;
-    }
-    int main () {
-        int n;
-        cin >> n;
-        vector<int> v(n);
-
-        for (int i=0; i<v.size(); i++) {
-            cin >> v[i];
         }
-        bubblesort(v);
-
-        for (int i=0; i<v.size(); i++) {
-            cout << v[i] << " ";
-        }
-
-            
-        return 0;
-    }
