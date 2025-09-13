afb2133c6733abdbe2bc7c19a8a7aec0add6aac6
AbdulM0izz <abdulmoiz95972@gmail.com>
2025-09-14 00:08:51 +0500

    Greater ele

---
+++ OOP/Greater.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+vector<int> nextGreaterElements(vector<int>& arr) {
+    int n = arr.size();
+    vector<int> result(n, -1); // Default -1
+    stack<int> st; // stack me index store karenge
+
+    for (int i = 0; i < n; i++) {
+        // Jab tak current element stack ke top se bada hai
+        while (!st.empty() && arr[i] > arr[st.top()]) {
+            result[st.top()] = arr[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+    return result;
+}
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    vector<int> arr(n);
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    vector<int> ans = nextGreaterElements(arr);
+
+    cout << "Next Greater Elements: ";
+    for (int x : ans) cout << x << " ";
+    cout << endl;
+
+    return 0;
+}
--- Sorting-Algorithms/Quicksort.cpp
+++ Sorting-Algorithms/Quicksort.cpp
@@ -2,28 +2,28 @@
 using namespace std;
 
 // Function to partition the array
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high]; // Choose last element as pivot
-    int i = low - 1;       // Pointer for smaller elements
+// int partition(int arr[], int low, int high) {
+//     int pivot = arr[high]; // Choose last element as pivot
+//     int i = low - 1;       // Pointer for smaller elements
 
-    for (int j = low; j < high; j++) {
-        if (arr[j] < pivot) { // If current element < pivot
-            i++;
-            swap(arr[i], arr[j]);
-        }
-    }
-    swap(arr[i + 1], arr[high]); // Place pivot in correct position
-    return i + 1;
-}
+//     for (int j = low; j < high; j++) {
+//         if (arr[j] < pivot) { // If current element < pivot
+//             i++;
+//             swap(arr[i], arr[j]);
+//         }
+//     }
+//     swap(arr[i + 1], arr[high]); // Place pivot in correct position
+//     return i + 1;
+// }
 
-// QuickSort function
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high); // Partition index
-        quickSort(arr, low, pi - 1);        // Sort left subarray
-        quickSort(arr, pi + 1, high);       // Sort right subarray
-    }
-}
+// // QuickSort function
+// void quickSort(int arr[], int low, int high) {
+//     if (low < high) {
+//         int pi = partition(arr, low, high); // Partition index
+//         quickSort(arr, low, pi - 1);        // Sort left subarray
+//         quickSort(arr, pi + 1, high);       // Sort right subarray
+//     }
+// }
 
 int main() {
     int arr[] = {10, 7, 8, 9, 1, 5};
